20ebc7e365630dbe574bf004251bace07f8913cb
ArtRegina <reginalefler@yandex.ru>
2022-05-28 20:01:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
 
 function App() {
-  const [value, setValue] = useState("text");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-     <ClassCounter/>
+      <form>
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <button>создать пост</button>
+      </form>
+      <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
